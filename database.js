43e23fa3697278943e4fd03489353b42ff466d04
TaliesinY <148133190+TaliesinY@users.noreply.github.com>
2024-05-25 12:13:13 -0400

    Delete database.js

--- database.js
+++
@@ -1,9 +0,0 @@
-const sqlite3 = require('sqlite3').verbose();
-
-const db = new sqlite3.Database(':memory:');
-
-db.serialize(() => {
-  db.run("CREATE TABLE form_responses (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, email TEXT, comment TEXT)");
-});
-
-module.exports = db;
